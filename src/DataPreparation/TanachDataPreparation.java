5f3406eb1be19972e9a299e2333abb64290a40ee
Rafi Levy <rafi.levy@sky.com>
2020-03-23 01:12:22 +0000

    Initial commit

---
+++ src/DataPreparation/TanachDataPreparation.java
@@ -0,0 +1,48 @@
+package DataPreparation;
+
+import Enums.TanachBook;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TanachDataPreparation {
+
+    public static void DownloadRawData() throws IOException {
+        for (TanachBook book : TanachBook.values()) {
+            Path destPath = book.getFilePath();
+            URL sourceURL = book.getSefariaURL();
+            try (InputStream inputStream = sourceURL.openStream()) {
+                Files.copy(inputStream, destPath, StandardCopyOption.REPLACE_EXISTING);
+            }
+        }
+    }
+
+    public static void CleanupTanachFiles() throws IOException {
+        for (TanachBook book : TanachBook.values()) {
+            Path filePath = book.getFilePath();
+            List<String> lines = Files.readAllLines(filePath);
+            lines = lines.subList(9 ,lines.size());
+            lines = lines.stream().filter((String line) -> !(line.contains("Chapter") || line.isEmpty())).collect(Collectors.toList());
+            Files.write(filePath, lines);
+        }
+    }
+
+    public static List<String> getLines(TanachBook book) throws IOException {
+        return Files.readAllLines(book.getFilePath());
+    }
+
+    public static List<String> getLines() throws IOException {
+        ArrayList<String> lines = new ArrayList<>();
+        for (TanachBook book : TanachBook.values()) lines.addAll(Files.readAllLines(book.getFilePath()));
+        return lines;
+    }
+
+    public static void main(String[] args) throws IOException {
+        DownloadRawData();
+        CleanupTanachFiles();
+    }
+}
